fc721afcbc325aec3e0c791cec0cd4d0e271de98
Brandon Guo <blg19@duke.edu>
2018-02-25 22:16:40 -0500

    things

--- src/TreeReader/ReadTree.java
+++ src/TreeReader/ReadTree.java
@@ -1,7 +1,9 @@
 package TreeReader;
 
+import java.util.Map;
+
 public class ReadTree {
-	public double evaluate(SlogoNode node) {
-		return node.execute();
+	public double evaluate(SlogoNode node, Map<String, Double> VarMap, Object turtle) {
+		return node.getExecute(VarMap, turtle);
 	}
 }
