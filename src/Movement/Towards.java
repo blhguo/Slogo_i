f08d1ac1d5f70cf418724264a5c0639128e6b7da
Brandon Guo <blg19@duke.edu>
2018-02-26 21:48:05 -0500

    created several Command Nodes and made significant changes to the overall Node structure

--- src/Movement/Towards.java
+++ src/Movement/Towards.java
@@ -5,24 +5,23 @@ import java.util.Map;
 
 import treenode.SlogoNode;
 
-public class Forward extends SlogoNode{
-	
+public class Towards extends SlogoNode{
+//	
 //	private double value = 0;
 //	private double distance = 0;
 //	public Forward() {
 //		this.val = getValue();
 //	}
 
-	private void forward(Object turtle, double distance) {
-		turtle.setX(turtle.getX() + distance * Math.sin(turtle.getheading())); //TODO: Update according to Jamie's stuff
-		turtle.setY(turtle.getY() + distance * Math.cos(turtle.getheading())); //TODO: Update according to Jamie's stuff
+	private void setHead(Object turtle, double angle) {
+		turtle.setheading(angle);
 	}
 
 	@Override
 	public double getExecute(Map<String, Double> VarMap,  Map<String, SlogoNode> FunctMap, Object turtle) {
 		// TODO Auto-generated method stub
 		double step = getValue(VarMap, FunctMap, turtle);
-		forward(turtle, step);
+		setHead(turtle, step);
 		return step;  //returns the final value of the node
 	}
 
@@ -30,8 +29,15 @@ public class Forward extends SlogoNode{
 	@Override
 	public double getValue(Map<String,Double> VarMap, Map<String, SlogoNode> FunctMap, Object turtle) {
 		// TODO Auto-generated method stub
+		double initial = turtle.getHeading(); //TODO: Update according to Jamie's stuff
+		double CurX = turtle.getX();
+		double CurY = turtle.getY();
 		List<SlogoNode> leaf = this.getChildren();
-		return leaf.get(0).getValue(VarMap, FunctMap, turtle);
+		double xpos = leaf.get(0).getValue(VarMap, FunctMap, turtle);
+		double ypos = leaf.get(1).getValue(VarMap, FunctMap, turtle);
+		
+		double newhead = Math.tan((xpos - CurX)/(ypos - CurY));
+		return newhead - initial;
 	}
 	
 }
--- src/TreeReader/ReadTree.java
+++ src/TreeReader/ReadTree.java
@@ -2,8 +2,10 @@ package TreeReader;
 
 import java.util.Map;
 
+import treenode.SlogoNode;
+
 public class ReadTree {
-	public double evaluate(SlogoNode node, Map<String, Double> VarMap, Object turtle) {
-		return node.getExecute(VarMap, turtle);
+	public double evaluate(SlogoNode node, Map<String, Double> VarMap, Map<String, SlogoNode> funct, Object turtle) {
+		return node.getExecute(VarMap, funct, turtle);
 	}
 }
