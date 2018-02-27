04049e2abbfb30cab12d90db0e315b590c418ed5
Jamie <nk143@duke.edu>
2018-02-27 02:49:52 -0500

    added some methods

--- src/turtle/Turtle.java
+++ src/turtle/Turtle.java
@@ -1,6 +1,9 @@
 package turtle;
 
 import javafx.geometry.Point2D;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 /**
 *
 * Turtle holds knowledge about themselves that can be
@@ -8,61 +11,117 @@ import javafx.geometry.Point2D;
 * 
 */
 public class Turtle {
-	private Point2D location;
-	private double speed;
+	private double myX;
+	private double myY;
+	private Point2D myLocation;
+	private double mySpeed;
 	private double heading;
-
+	private boolean isShowing;
+	private boolean myPenUp;
+	private Canvas myCanvas;
+	
+	private ImageView image;
 	/**
 	 * Variety of getters and setters used to access Information in actors, for
 	 * update and display
 	 **/
 	public Turtle() {
-		this(new Point2D(0, 0), 0);
+		this(0, 0, new Point2D(0, 0), 0);
+		myCanvas = new Canvas();
+
 	}
 
 
-	public Turtle(Point2D location, double speed)
-	{
-		this.location = location;
-		this.speed = speed;
+	public Turtle(double xPosition, double yPosition, Point2D location, double speed) {
+		this.myX = xPosition;
+		this.myY = yPosition;
+		this.myLocation = location;
+		this.mySpeed = speed;
+
 	}
 
+	public double getX(double xPosition) {
+		return myX;
+	}
+	
+	public double getY(double yPosition) {
+		return myY;
+	}
+
+	public void setX(double xPosition) {
+		myX = xPosition;
+	}
+	
+	public void setY(double yPosition) {
+		myY = yPosition;
+	}
+	
 	/**
-	 * Point representing the current location of the actor
+	 * Point representing the current location of the turtle
 	 * 
 	 * @return Point location
 	 */
-	public Point2D getLocation()
-	{
-		return location;
+	public Point2D getLocation() {
+		return myLocation;
 	}
 
-	public void setLocation(Point2D p)
-	{
-		location = p;
+	public void setLocation(Point2D p) {
+		myLocation = p;
 	}
 
 	/**
-	 * An int representing the speed of the actor as it updates on the screen
+	 * An int representing the speed of the turtle as it updates on the screen
 	 * 
 	 */
-	public double getSpeed()
-	{
-		return speed;
+	public double getSpeed() {
+		return mySpeed;
 	}
 
-	public void setSpeed(double s)
-	{
-		speed = s;
+	public void setSpeed(double s) {
+		mySpeed = s;
 	}
 
-	public double getHeading()
-	{
+	public double getHeading() {
 		return heading;
 	}
 
-	public void setHeading(double heading)
-	{
+	public void setHeading(double heading) {
 		this.heading = Math.floorMod((int) heading, 360);
 	}
+	
+	public void rotate(double angle) {
+		setHeading(heading + angle);
+	}
+	
+	public void penUp() {
+		myPenUp = true;
+	}
+	
+	public void penDown() {
+		myPenUp = false;
+	}
+	
+	public Image show() {
+		isShowing = true;
+		return image.getImage();
+	}
+	
+	public void hide() {
+		isShowing = false;
+		myCanvas.getGraphicsContext2D().clearRect(0, 0, myCanvas.getWidth(), myCanvas.getHeight());
+	}
+	
+	public boolean isShowing() {
+		return isShowing;
+	}
+
+	public void update() {
+		
+	}
+	
+	public void reset() {
+		myCanvas.getGraphicsContext2D().clearRect(0, 0, myCanvas.getWidth(), myCanvas.getHeight());
+	}
+	
+
 }
