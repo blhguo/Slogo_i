084d8aa9f6de5247f9a9d3521c0daa558b03da5e
Jamie <nk143@duke.edu>
2018-02-28 01:05:46 -0500

    merging

--- src/Movement/Backward.java
+++ src/Movement/Backward.java
@@ -1,6 +1,5 @@
 package Movement;
 
-
 import java.util.List;
 import java.util.Map;
 
@@ -8,43 +7,34 @@ import javafx.geometry.Point2D;
 import treenode.SlogoNode;
 import turtle.Turtle;
 
-public class SetPosition extends SlogoNode{
-//	
+public class Backward extends SlogoNode{
+	
 //	private double value = 0;
 //	private double distance = 0;
 //	public Forward() {
 //		this.val = getValue();
 //	}
 
-	private double[] location;
-	
-	private void setpos(Turtle turtle, double[] loc) {
-        Point2D newpos = new Point2D(loc[0], loc[1]);
-        turtle.setLocation(newpos);
-
+	private void backward(Turtle turtle, double distance) {
+		Point2D point = new Point2D(turtle.getLocation().getX() - distance * Math.sin(turtle.getHeading()),
+				turtle.getLocation().getY() - distance * Math.cos(turtle.getHeading()));
+		turtle.setLocation(point);
 	}
 
 	@Override
 	public double getExecute(Map<String, Double> VarMap,  Map<String, SlogoNode> FunctMap, Turtle turtle) {
 		// TODO Auto-generated method stub
 		double step = getValue(VarMap, FunctMap, turtle);
-		setpos(turtle, location);
+		backward(turtle, step);
 		return step;  //returns the final value of the node
 	}
-	
+
+
 	@Override
 	public double getValue(Map<String,Double> VarMap, Map<String, SlogoNode> FunctMap, Turtle turtle) {
 		// TODO Auto-generated method stub
-		//TODO: Update according to Jamie's stuff
-		double CurX = turtle.getLocation().getX();
-		double CurY = turtle.getLocation().getY();
 		List<SlogoNode> leaf = this.getChildren();
-		double xpos = leaf.get(0).getValue(VarMap, FunctMap, turtle);
-		double ypos = leaf.get(1).getValue(VarMap, FunctMap, turtle);
-		location[0] = xpos;
-		location[1] = ypos;
-		double distance = Math.pow(Math.pow(xpos - CurX, 2) + Math.pow(ypos - CurY, 21), 0.5);
-		return distance;
+		return leaf.get(0).getValue(VarMap, FunctMap, turtle);
 	}
 	
 }
