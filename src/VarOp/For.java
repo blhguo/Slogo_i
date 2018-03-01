2a866739311a59cb36fb298a0d128d4cc679228e
Jamie <nk143@duke.edu>
2018-03-01 00:21:57 -0500

    added if and ifelse

--- src/VarOp/For.java
+++ src/VarOp/For.java
@@ -1,5 +1,6 @@
 package VarOp;
 
+import java.util.List;
 import java.util.Map;
 
 import treenode.SlogoNode;
@@ -18,8 +19,19 @@ public class For extends SlogoNode{
 	}
 
 	@Override
-	public double getValue(Map<String, Double> VarMap, Map<String, SlogoNode> funct, Turtle turtle) {
-		// TODO Auto-generated method stub
+	public double getValue(Map<String, Double> VarMap, Map<String, SlogoNode> FunctMap, Turtle turtle) {
+		List<SlogoNode> leaf = this.getChildren();
+		double variable = leaf.get(0).getValue(VarMap, FunctMap, turtle);
+		double start = leaf.get(1).getValue(VarMap, FunctMap, turtle);
+		double end = leaf.get(2).getValue(VarMap, FunctMap, turtle);
+		double increment = leaf.get(3).getValue(VarMap, FunctMap, turtle);
+		SlogoNode command = leaf.get(4);
+		if (command != null) {
+			for (double i = start; i < end; i+= increment) {
+				command.getExecute(VarMap, FunctMap, turtle);
+			}
+			return command.getValue(VarMap, FunctMap, turtle);
+		}
 		return 0;
 	}
 
