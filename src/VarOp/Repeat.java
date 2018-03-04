4db58b41dba1961e521d808d53db279471e8e64a
Ryan Fu <rwf11@duke.edu>
2018-03-04 17:49:25 -0500

    starting To function

--- src/VarOp/Repeat.java
+++ src/VarOp/Repeat.java
@@ -1,5 +1,22 @@
 package VarOp;
 
-public class Repeat {
+import java.util.Map;
+
+import treenode.SlogoNode;
+import turtle.Turtle;
+
+public class Repeat extends SlogoNode{
+
+	@Override
+	public double getExecute(Map<String, Double> VarMap, Map<String, SlogoNode> FunctMap, Turtle turtle) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getValue(Map<String, Double> VarMap, Map<String, SlogoNode> funct, Turtle turtle) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
